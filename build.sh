6a68b387b50bf289f09180e4f7a8d95e7768e285
Zach M <userunknown00@gmail.com>
2016-11-28 09:23:56 -0700

    Fixed copy script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 mkdir ./build
-cp ./* ./build -r
+
+# The copy method below throws an error regarding recursive copying when executed. Switched to rsync to avoid, but left the old command in for review.
+# cp ./* ./build -r
+
+rsync -av --exclude './build' ./* ./build
 cp "$(which node)" ./build/
 rm ./build/build.sh
