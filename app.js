b0afabdcba10c6fb8def9e53b25ff713f15edbb5
jpfeiffer16 <jpfeiffer@bluemodus.com>
2016-11-29 17:33:39 -0700

    Getting reday to add udp address

--- app.js
+++ app.js
@@ -25,24 +25,18 @@ if  (program.ngrok) {
     if (err) throw err;
     console.log('Tunnel at: \n%s . Press any key to enter the app.', url);
     let stdin = process.stdin;
-    // without this, we would only get streams once enter is pressed
     stdin.setRawMode( true );
 
-    // resume stdin in the parent process (node app won't quit all by itself
-    // unless an error or process.exit() happens)
     stdin.resume();
 
-    // i don't want binary, do you?
     stdin.setEncoding( 'utf8' );
 
-    // on any data into stdin
     stdin.on('data', function( key ){
       // ctrl-c ( end of text )
       if ( key === '\u0003' ) {
         process.exit();
       }
-      // write the key to stdout all normal like
-      // process.stdout.write( key );
+      
       stdin.setRawMode(false);
       stdin.removeAllListeners();
       main();
